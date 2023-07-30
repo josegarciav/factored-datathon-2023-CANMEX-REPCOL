e86d31d5c45b94bceb5a32b6f16964e6a2ed5ea9
Jose Garcia <jose.garcia.phd@gmail.com>
2023-07-30 17:32:09 -0400

    changing the requirements file

--- app/app.py
+++ app/app.py
@@ -13,35 +13,57 @@ from sklearn.manifold import TSNE
 
 
 df_merged = pd.read_parquet("df_merged.parquet")
+import streamlit as st
+
+# Define all your pages
+def page1():
+    st.title("Data")
+    st.write("Welcome to Data Page")
+
+def page2():
+    st.title("t-SNE")
+    st.write("Welcome to t-SNE Page")
+
+    if st.button('Run t-SNE'):
+
+        # Select the features
+        features = df_merged[['overall','reviewText_sentiment','summary_sentiment','average_price','season']]
+
+        # Apply the t-SNE transformation
+        tsne = TSNE(n_components=3, random_state=0)
+        tsne_results = tsne.fit_transform(features)
+
+        # Create a 3D scatter plot
+        fig = go.Figure(data=[go.Scatter3d(
+            x=tsne_results[:, 0],
+            y=tsne_results[:, 1],
+            z=tsne_results[:, 2],
+            mode='markers',
+            marker=dict(
+                size=2,
+                color=df_merged['overall'],  # set color to season
+                colorscale='Viridis',  # choose a colorscale
+                colorbar=dict(title="Overall review"),  # colorbar title
+                opacity=0.8
+            )
+        )])
+
+        st.plotly_chart(fig)
 
 
-# Add a button for running t-SNE
-if st.button('Run t-SNE'):
+def main():
+    st.sidebar.title("Navigation")
+    selection = st.sidebar.radio("Go to", ["Page 1", "Page 2"]) # Add more pages here
 
-    # Select the features
-    features = df_merged[['overall','reviewText_sentiment','summary_sentiment','average_price','season']]
+    if selection == "Page 1":
+        page1()
+    elif selection == "Page 2":
+        page2()
+    # Add more pages here in elif statements
 
-    # Apply the t-SNE transformation
-    tsne = TSNE(n_components=3, random_state=0)
-    tsne_results = tsne.fit_transform(features)
+if __name__ == "__main__":
+    main()
 
-    # Create a 3D scatter plot
-    fig = go.Figure(data=[go.Scatter3d(
-        x=tsne_results[:, 0],
-        y=tsne_results[:, 1],
-        z=tsne_results[:, 2],
-        mode='markers',
-        marker=dict(
-            size=2,
-            color=df_merged['season'],  # set color to season
-            colorscale='Viridis',  # choose a colorscale
-            colorbar=dict(title="Seasons"),  # colorbar title
-            opacity=0.8
-        )
-    )])
-
-    # Display the figure in Streamlit
-    st.plotly_chart(fig)
 
 
 st.write('Hello world!')
